f0d190519ce090de14441cd6dfbb1bd2bee4c1d4
Pc <kevsertemur@gmail.com>
2023-01-03 20:42:01 +0300

    testNG

--- src/test/java/clarusway/tests/HW_dependsOn.java
+++ src/test/java/clarusway/tests/HW_dependsOn.java
@@ -1,6 +1,5 @@
-package clarusway.test;
+package clarusway.tests;
 
-import clarusway.utilities.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
