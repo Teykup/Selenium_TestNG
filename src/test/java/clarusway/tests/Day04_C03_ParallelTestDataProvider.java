ae872e20684ed0b79d14fa6c57754b31dd339d86
Pc <kevsertemur@gmail.com>
2023-01-03 21:50:28 +0300

    testNG

---
+++ src/test/java/clarusway/tests/Day04_C03_ParallelTestDataProvider.java
@@ -0,0 +1,23 @@
+package clarusway.tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Day04_C03_ParallelTestDataProvider {
+
+    @Test(dataProvider = "getData")
+    public void test (String name, String surname){
+        System.out.println(name + " " + surname);
+
+    }
+
+
+    @DataProvider(parallel = true)
+    public Object [][] getData(){
+        return new Object[][]{
+                {"yusuf", "çelik"},
+                {"can", "efe"},
+                {"merve", "aslan"}
+        };
+    }
+}
