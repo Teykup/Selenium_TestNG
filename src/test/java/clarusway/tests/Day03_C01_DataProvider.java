f86a03f77a72ef930701fa300c8221570785409d
Pc <kevsertemur@gmail.com>
2023-01-05 21:11:21 +0300

    testNG

---
+++ src/test/java/clarusway/tests/Day03_C01_DataProvider.java
@@ -0,0 +1,24 @@
+package clarusway.tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Day03_C01_DataProvider {
+
+
+    @Test(dataProvider = "dataProvider")
+    public void test01(String name){
+        System.out.println(name);
+    }
+
+
+    @DataProvider
+    public Object[][] dataProvider(){
+        Object[][] data = {
+                {"kevser"},
+                {"temür"},
+                {"hadiOrdan"}
+        };
+        return data;
+    }
+}
