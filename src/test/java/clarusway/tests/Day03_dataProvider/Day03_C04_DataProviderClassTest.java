b23109934051f1967bc9dd67191b60f5707837c5
Pc <kevsertemur@gmail.com>
2023-01-05 21:11:54 +0300

    testNG

---
+++ src/test/java/clarusway/tests/Day03_dataProvider/Day03_C04_DataProviderClassTest.java
@@ -0,0 +1,16 @@
+package clarusway.tests.Day03_dataProvider;
+
+import org.testng.annotations.Test;
+
+public class Day03_C04_DataProviderClassTest {
+
+    @Test(
+            dataProvider = "testData",
+            dataProviderClass = Day03_C03_DataProviderClass.class
+    )
+    public void dataProviderTest(String english, String turkish){
+
+        System.out.println(english);
+        System.out.println(turkish);
+    }
+}
